62d9e5405d6b8cb6a5725a39c3564920fb3b0f2f
bradxy <36649225+bradxy@users.noreply.github.com>
2019-07-27 19:22:11 -0700

    better client permissions

--- pptp_vrp2/client.lua
+++ pptp_vrp2/client.lua
@@ -18,31 +18,25 @@ Citizen.CreateThread(function()
 		if DoesEntityExist( ped ) and not IsEntityDead( ped ) then
 			if not IsPauseMenuActive() then 
 				loadAnimDict( "random@arrests" )
-				if IsControlJustReleased( 0, 246 ) then -- INPUT_CHARACTER_WHEEL (LEFT ALT)
-				self.remote._ownedVhicleCheck() -- permission check for personal vehicle
-
-				if usageAllowed then
+				if IsControlJustReleased( 0, 246 ) and usageAllowed then -- INPUT_CHARACTER_WHEEL (LEFT ALT)
+				
+				vRP.EXT.Emotes2:clearEmotes()
 					--TriggerServerEvent('InteractSound_SV:PlayOnSource', 'off', 0.1)
 					ClearPedTasks(ped)
 					SetEnableHandcuffs(ped, false)
-				end
 				else
-					if IsControlJustPressed( 0, 246 ) and not IsPlayerFreeAiming(PlayerId()) then -- INPUT_CHARACTER_WHEEL (LEFT ALT)
-						self.remote._ownedVhicleCheck() -- permission check for personal vehicle
+					if IsControlJustPressed( 0, 246 ) and not IsPlayerFreeAiming(PlayerId()) and usageAllowed then -- INPUT_CHARACTER_WHEEL (LEFT ALT)
 
-						if usageAllowed then
+						vRP.EXT.Emotes2:clearEmotes()
 						--TriggerServerEvent('InteractSound_SV:PlayOnSource', 'on', 0.1)
 						TaskPlayAnim(ped, "random@arrests", "generic_radio_enter", 8.0, 2.0, -1, 50, 2.0, 0, 0, 0 )
-						end
-					elseif IsControlJustPressed( 0, 246 ) and IsPlayerFreeAiming(PlayerId()) then -- INPUT_CHARACTER_WHEEL (LEFT ALT)
-						self.remote._ownedVhicleCheck() -- permission check for personal vehicle
+					elseif IsControlJustPressed( 0, 246 ) and IsPlayerFreeAiming(PlayerId()) and usageAllowed then -- INPUT_CHARACTER_WHEEL (LEFT ALT)
 
-						if usageAllowed then
+						vRP.EXT.Emotes2:clearEmotes()
 						--TriggerServerEvent('InteractSound_SV:PlayOnSource', 'on', 0.1)
 						TaskPlayAnim(ped, "random@arrests", "radio_chatter", 8.0, 2.0, -1, 50, 2.0, 0, 0, 0 )
 						SetEnableHandcuffs(ped, true)
 
-					end 
 					end
 					if IsEntityPlayingAnim(GetPlayerPed(PlayerId()), "random@arrests", "generic_radio_enter", 3) then
 						DisableActions(ped)
@@ -63,30 +57,25 @@ end )
 Citizen.CreateThread( function()
 	while true do 
 		Citizen.Wait( 1 )
-		      if self.state_ready then
-		HideHudComponentThisFrame(9) -- Street Name
+		if self.state_ready then
 		local ped = PlayerPedId()
-		if DoesEntityExist( ped ) and not IsEntityDead( ped ) and not IsPedInAnyVehicle(PlayerPedId(), true) then 
+		if DoesEntityExist( ped ) and not IsEntityDead( ped ) and not IsPedInAnyVehicle(PlayerPedId(), true) and usageAllowed then 
 			DisableControlAction( 0, 20, true ) -- INPUT_MULTIPLAYER_INFO (Z)
 			if not IsPauseMenuActive() then 
 
 				loadAnimDict( "reaction@intimidation@cop@unarmed" )		
 				if IsDisabledControlJustReleased( 0, 20 ) then -- INPUT_MULTIPLAYER_INFO (Z)
-				self.remote._ownedVhicleCheck() -- permission check for personal vehicle
-				if usageAllowed then
+				vRP.EXT.Emotes2:clearEmotes()
 					ClearPedTasks(ped)
 					SetEnableHandcuffs(ped, false)
 					SetCurrentPedWeapon(ped, GetHashKey("WEAPON_UNARMED"), true)
-					end
 				else
-					if IsDisabledControlJustPressed( 0, 20 ) then -- INPUT_MULTIPLAYER_INFO (Z)
-						self.remote._ownedVhicleCheck() -- permission check for personal vehicle
+					if IsDisabledControlJustPressed( 0, 20 ) and usageAllowed then -- INPUT_MULTIPLAYER_INFO (Z)
 
-						if usageAllowed then
+						vRP.EXT.Emotes2:clearEmotes()
 						SetEnableHandcuffs(ped, true)
 						SetCurrentPedWeapon(ped, GetHashKey("WEAPON_UNARMED"), true) 
 						TaskPlayAnim(ped, "reaction@intimidation@cop@unarmed", "intro", 8.0, 2.0, -1, 50, 2.0, 0, 0, 0 )
-						end
 					end
 					if IsEntityPlayingAnim(GetPlayerPed(PlayerId()), "reaction@intimidation@cop@unarmed", "intro", 3) then 
 						DisableActions(ped)
@@ -103,27 +92,26 @@ end )
  Citizen.CreateThread(function()
 	while true do
 		Citizen.Wait(1)
-		      if self.state_ready then
+		if self.state_ready then
 		local ped = PlayerPedId()
-		if DoesEntityExist( ped ) and not IsEntityDead( ped ) and not IsPedInAnyVehicle(PlayerPedId(), true)  then
+		if DoesEntityExist( ped ) and not IsEntityDead( ped ) and not IsPedInAnyVehicle(PlayerPedId(), true) and usageAllowed then
 			loadAnimDict( "rcmjosh4" )
 			loadAnimDict( "weapons@pistol@" )
 			if CheckWeapon(ped) then
 				if holstered then
-				self.remote._ownedVhicleCheck() -- permission check for personal vehicle
 
-				if usageAllowed then
+				vRP.EXT.Emotes2:clearEmotes()
 					TaskPlayAnim(ped, "rcmjosh4", "josh_leadout_cop2", 8.0, 2.0, -1, 48, 10, 0, 0, 0 )
 					Citizen.Wait(600)
 					ClearPedTasks(ped)
 					holstered = false
-					end
 				end
 				--SetPedComponentVariation(ped, 9, 0, 0, 0)
-			elseif not CheckWeapon(ped) then
+			elseif not CheckWeapon(ped) and usageAllowed then
 				if not holstered then
-				self.remote._ownedVhicleCheck() -- permission check for personal vehicle
-				if usageAllowed then
+				
+
+				vRP.EXT.Emotes2:clearEmotes()
 					TaskPlayAnim(ped, "weapons@pistol@", "aim_2_holster", 8.0, 2.0, -1, 48, 10, 0, 0, 0 )
 					Citizen.Wait(500)
 					ClearPedTasks(ped)
@@ -131,7 +119,6 @@ end )
 					end
 				end
 				--SetPedComponentVariation(ped, 9, 1, 0, 0)
-				end
 			end
 		end
 	end
@@ -177,19 +164,19 @@ function loadAnimDict( dict )
 	end
 end
 
-function PPtp:blockEnter(blockEnter)
-	usageAllowed = not blockEnter
-	
-end
+
 
 	
 -- TUNNEL
 PPtp.tunnel = {}
 
+function PPtp.tunnel:checkPermission(flag)
+	usageAllowed = flag
+	
+end
+
 function PPtp.tunnel:setStateReady(state)
   self.state_ready = state
 end
-
-PPtp.tunnel.blockEnter = PPtp.blockEnter
 
 vRP:registerExtension(PPtp)	
--- pptp_vrp2/server.lua
+++ pptp_vrp2/server.lua
@@ -6,31 +6,30 @@ local PPtp = class("PPtp", vRP.Extension)
   
 function PPtp:__construct()
     vRP.Extension.__construct(self)
-
-
 end
 
 PPtp.event = {}
+	
 
 function PPtp.event:playerStateLoaded(user)
- 
-  self.remote._setStateReady(user.source, true)
+if user and user:isReady() then
+self.remote._setStateReady(user.source, true) --only allow to activate when user state is ready
+    if user:hasPermission("police.market") then
+        self.remote._checkPermission(user.source, true)
+    else
+        self.remote._checkPermission(user.source, false)
+		end
+    end
 end
-	
--- TUNNEL
-PPtp.tunnel = {}
 
-function PPtp.tunnel:ownedVhicleCheck()
-  local blockEnter = false
-  local user = vRP.users_by_source[source]
-
-  if user:hasPermission("police.pc") then
-  self.remote._blockEnter(user.source,blockEnter)
-  else
-    local blockEnter = true
-		self.remote._blockEnter(user.source,blockEnter)
-	end
-
+function PPtp.event:playerJoinGroup(user)
+    if user:hasPermission("police.market") then
+        self.remote._checkPermission(user.source, true)
+    else
+        self.remote._checkPermission(user.source, false)
+    end
 end
+
+
 
 vRP:registerExtension(PPtp)	
